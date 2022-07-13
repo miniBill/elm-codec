f8129eb2fa5f7c62f74b8dc95a0fd5827d7bc0d9
pravdomil <pravdomil.toman@gmail.com>
2022-07-13 18:39:27 +0200

    tests

--- tests/Base.elm
+++ tests/Base.elm
@@ -1,312 +1,234 @@
-module Base exposing (roundtrips, suite)
+module Base exposing (..)
 
-import Codec exposing (Codec)
+import Codec
 import Dict
 import Expect
-import Fuzz exposing (Fuzzer)
+import Fuzz
 import Set
-import Test exposing (Test, describe, fuzz, test)
+import Test
 
 
-suite : Test
+suite : Test.Test
 suite =
-    describe "Testing roundtrips"
-        [ describe "Basic" basicTests
-        , describe "Containers" containersTests
-        , describe "Object" objectTests
-        , describe "Custom" customTests
-        , describe "bimap" bimapTests
-        , describe "maybe" maybeTests
-        , describe "succeed"
-            [ test "roundtrips"
-                (\_ ->
-                    Codec.succeed 632
-                        |> (\d -> Codec.decodeString d "{}")
-                        |> Expect.equal (Ok 632)
+    Test.describe "Testing codecs."
+        [ tuplesTest
+        , recordsTest
+        , customTypesTest
+        , opaqueCustomTypesTest
+        , commonCustomTypesTest
+        , helperTest
+        ]
+
+
+tuplesTest : Test.Test
+tuplesTest =
+    Test.describe "Tuples."
+        [ Test.describe "Codec.tuple"
+            [ roundTripTest
+                (Fuzz.tuple ( Fuzz.int, Fuzz.int ))
+                (Codec.tuple Codec.int Codec.int)
+            ]
+        , Test.describe "Codec.tuple3"
+            [ roundTripTest
+                (Fuzz.tuple3 ( Fuzz.int, Fuzz.int, Fuzz.int ))
+                (Codec.tuple3 Codec.int Codec.int Codec.int)
+            ]
+        ]
+
+
+recordsTest : Test.Test
+recordsTest =
+    Test.describe "Records."
+        [ Test.describe "with 0 fields"
+            [ roundTripTest
+                (Fuzz.constant {})
+                (Codec.record {}
+                    |> Codec.buildRecord
                 )
             ]
-        , describe "recursive" recursiveTests
-        , describe "map,andThen" mapAndThenTests
-        ]
-
-
-roundtrips : Fuzzer a -> Codec a -> Test
-roundtrips fuzzer codec =
-    fuzz fuzzer "is a roundtrip" <|
-        \value ->
-            value
-                |> Codec.encoder codec
-                |> Codec.decodeValue codec
-                |> Expect.equal (Ok value)
-
-
-roundtripsWithin : Fuzzer Float -> Codec Float -> Test
-roundtripsWithin fuzzer codec =
-    fuzz fuzzer "is a roundtrip" <|
-        \value ->
-            value
-                |> Codec.encoder codec
-                |> Codec.decodeValue codec
-                |> Result.withDefault -999.1234567
-                |> Expect.within (Expect.Relative 0.000001) value
-
-
-basicTests : List Test
-basicTests =
-    [ describe "Codec.string"
-        [ roundtrips Fuzz.string Codec.string
-        ]
-    , describe "Codec.int"
-        [ roundtrips Fuzz.int Codec.int
-        ]
-    , describe "Codec.float"
-        [ roundtrips Fuzz.float Codec.float
-        ]
-    , describe "Codec.bool"
-        [ roundtrips Fuzz.bool Codec.bool
-        ]
-    ]
-
-
-containersTests : List Test
-containersTests =
-    [ describe "Codec.array"
-        [ roundtrips (Fuzz.array Fuzz.int) (Codec.array Codec.int)
-        ]
-    , describe "Codec.list"
-        [ roundtrips (Fuzz.list Fuzz.int) (Codec.list Codec.int)
-        ]
-    , describe "Codec.dict"
-        [ roundtrips
-            (Fuzz.map2 Tuple.pair Fuzz.string Fuzz.int
-                |> Fuzz.list
-                |> Fuzz.map Dict.fromList
-            )
-            (Codec.dict Codec.int)
-        ]
-    , describe "Codec.set"
-        [ roundtrips
-            (Fuzz.list Fuzz.int |> Fuzz.map Set.fromList)
-            (Codec.set Codec.int)
-        ]
-    , describe "Codec.tuple"
-        [ roundtrips
-            (Fuzz.tuple ( Fuzz.int, Fuzz.int ))
-            (Codec.tuple Codec.int Codec.int)
-        ]
-    ]
-
-
-objectTests : List Test
-objectTests =
-    [ describe "with 0 fields"
-        [ roundtrips (Fuzz.constant {})
-            (Codec.record {}
-                |> Codec.buildRecord
-            )
-        ]
-    , describe "with 1 field"
-        [ roundtrips (Fuzz.map (\i -> { fname = i }) Fuzz.int)
-            (Codec.record (\i -> { fname = i })
-                |> Codec.field "fname" .fname Codec.int
-                |> Codec.buildRecord
-            )
-        ]
-    , describe "with 2 fields"
-        [ roundtrips
-            (Fuzz.map2
-                (\a b ->
-                    { a = a
-                    , b = b
-                    }
+        , Test.describe "with 1 field"
+            [ roundTripTest
+                (Fuzz.map
+                    (\a -> { a = a })
+                    Fuzz.int
                 )
-                Fuzz.int
-                Fuzz.int
-            )
-            (Codec.record
-                (\a b ->
-                    { a = a
-                    , b = b
-                    }
-                )
-                |> Codec.field "a" .a Codec.int
-                |> Codec.field "b" .b Codec.int
-                |> Codec.buildRecord
-            )
-        ]
-    , describe "nullableField vs maybeField" <|
-        let
-            nullableCodec =
-                Codec.record
-                    (\f -> { f = f })
-                    |> Codec.nullableField "f" .f Codec.int
+                (Codec.record
+                    (\a -> { a = a })
+                    |> Codec.field "a" .a Codec.int
                     |> Codec.buildRecord
-
-            maybeCodec =
-                Codec.record
-                    (\f -> { f = f })
-                    |> Codec.maybeField "f" .f Codec.int
+                )
+            ]
+        , Test.describe "with 2 fields"
+            [ roundTripTest
+                (Fuzz.map2
+                    (\a b -> { a = a, b = b })
+                    Fuzz.int
+                    Fuzz.int
+                )
+                (Codec.record
+                    (\a b -> { a = a, b = b })
+                    |> Codec.field "a" .a Codec.int
+                    |> Codec.field "b" .b Codec.int
                     |> Codec.buildRecord
-        in
-        [ test "a nullableField is required" <|
-            \_ ->
-                "{}"
-                    |> Codec.decodeString nullableCodec
-                    |> (\r ->
-                            case r of
-                                Ok _ ->
-                                    Expect.fail "Should have failed"
-
-                                Err _ ->
-                                    Expect.pass
-                       )
-        , test "a nullableField produces a field with a null value on encoding Nothing" <|
-            \_ ->
-                { f = Nothing }
-                    |> Codec.encodeToString 0 nullableCodec
-                    |> Expect.equal "{\"f\":null}"
-        , test "a maybeField is optional" <|
-            \_ ->
-                "{}"
-                    |> Codec.decodeString maybeCodec
-                    |> Expect.equal (Ok { f = Nothing })
-        , test "a maybeField doesn't produce a field on encoding Nothing" <|
-            \_ ->
-                { f = Nothing }
-                    |> Codec.encodeToString 0 maybeCodec
-                    |> Expect.equal "{}"
-        ]
-    ]
-
-
-type Newtype a
-    = Newtype a
-
-
-customTests : List Test
-customTests =
-    [ describe "with 1 ctor, 0 args"
-        [ roundtrips (Fuzz.constant ())
-            (Codec.custom
-                (\f v ->
-                    case v of
-                        () ->
-                            f
                 )
-                |> Codec.variant0 "()" ()
-                |> Codec.buildCustom
+            ]
+        , Test.describe "with maybeField"
+            (let
+                recordCodec : Codec.Codec { a : Maybe Int }
+                recordCodec =
+                    Codec.record
+                        (\a -> { a = a })
+                        |> Codec.maybeField "a" .a Codec.int
+                        |> Codec.buildRecord
+             in
+             [ roundTripTest
+                (Fuzz.map
+                    (\a -> { a = a })
+                    (Fuzz.maybe Fuzz.int)
+                )
+                recordCodec
+             , Test.test "maybeField is optional"
+                (\_ ->
+                    Expect.equal
+                        (Ok { a = Nothing })
+                        (Codec.decodeString recordCodec "{}")
+                )
+             , Test.test "maybeField doesn't produce a field on encoding Nothing"
+                (\_ ->
+                    Expect.equal
+                        "{}"
+                        (Codec.encodeToString 0 recordCodec { a = Nothing })
+                )
+             ]
             )
         ]
-    , describe "with 1 ctor, 1 arg"
-        [ roundtrips (Fuzz.map Newtype Fuzz.int)
-            (Codec.custom
-                (\f v ->
-                    case v of
-                        Newtype a ->
-                            f a
-                )
-                |> Codec.variant1 "Newtype" Newtype Codec.int
-                |> Codec.buildCustom
-            )
-        ]
-    , describe "with 2 ctors, 0,1 args" <|
-        let
-            match fnothing fjust value =
-                case value of
-                    Nothing ->
-                        fnothing
 
-                    Just v ->
-                        fjust v
 
-            codec =
-                Codec.custom match
-                    |> Codec.variant0 "Nothing" Nothing
-                    |> Codec.variant1 "Just" Just Codec.int
+customTypesTest : Test.Test
+customTypesTest =
+    Test.describe "Custom types."
+        [ Test.describe "with 1 variant, 0 args"
+            [ roundTripTest
+                (Fuzz.constant ())
+                (Codec.custom
+                    (\fn x ->
+                        case x of
+                            () ->
+                                fn
+                    )
+                    |> Codec.variant0 "()" ()
                     |> Codec.buildCustom
-
-            fuzzers =
-                [ ( "1st ctor", Fuzz.constant Nothing )
-                , ( "2nd ctor", Fuzz.map Just Fuzz.int )
-                ]
-        in
-        fuzzers
-            |> List.map
-                (\( name, fuzz ) ->
-                    describe name
-                        [ roundtrips fuzz codec ]
                 )
-    ]
+            ]
+        , Test.describe "with 1 variant, 1 arg"
+            [ roundTripTest
+                (Fuzz.map
+                    SomeType
+                    Fuzz.int
+                )
+                (Codec.custom
+                    (\fn x ->
+                        case x of
+                            SomeType a ->
+                                fn a
+                    )
+                    |> Codec.variant1 "SomeType" SomeType Codec.int
+                    |> Codec.buildCustom
+                )
+            ]
+        ]
 
 
-bimapTests : List Test
-bimapTests =
-    [ roundtripsWithin Fuzz.float <|
-        Codec.map
-            (\x -> x * 2)
-            (\x -> x / 2)
-            Codec.float
-    ]
+opaqueCustomTypesTest : Test.Test
+opaqueCustomTypesTest =
+    Test.describe "Opaque custom types."
+        [ Test.describe "Codec.int" [ roundTripTest Fuzz.int Codec.int ]
+        , Test.describe "Codec.float" [ roundTripTest Fuzz.float Codec.float ]
+        , Test.describe "Codec.char" [ roundTripTest Fuzz.char Codec.char ]
+        , Test.describe "Codec.string" [ roundTripTest Fuzz.string Codec.string ]
+        , Test.describe "Codec.list" [ roundTripTest (Fuzz.list Fuzz.int) (Codec.list Codec.int) ]
+        , Test.describe "Codec.array" [ roundTripTest (Fuzz.array Fuzz.int) (Codec.array Codec.int) ]
+        , Test.describe "Codec.dict" [ roundTripTest (dictFuzzer Fuzz.int Fuzz.string) (Codec.dict Codec.int Codec.string) ]
+        , Test.describe "Codec.set" [ roundTripTest (setFuzzer Fuzz.int) (Codec.set Codec.int) ]
+        ]
 
 
-maybeTests : List Test
-maybeTests =
-    [ describe "single"
-        [ roundtrips
-            (Fuzz.oneOf
-                [ Fuzz.constant Nothing
-                , Fuzz.map Just Fuzz.int
+commonCustomTypesTest : Test.Test
+commonCustomTypesTest =
+    Test.describe "Common custom types."
+        [ Test.describe "Bool."
+            [ roundTripTest
+                Fuzz.bool
+                Codec.bool
+            ]
+        , Test.describe "Maybe."
+            [ Test.describe "single"
+                [ roundTripTest
+                    (Fuzz.maybe Fuzz.int)
+                    (Codec.maybe Codec.int)
                 ]
+            , Test.describe "double"
+                [ roundTripTest
+                    (Fuzz.maybe (Fuzz.maybe Fuzz.int))
+                    (Codec.maybe (Codec.maybe Codec.int))
+                ]
+            ]
+        , Test.describe "Result."
+            [ roundTripTest
+                (Fuzz.result Fuzz.string Fuzz.int)
+                (Codec.result Codec.string Codec.int)
+            ]
+        ]
+
+
+helperTest : Test.Test
+helperTest =
+    Test.describe "Helper functions."
+        [ Test.describe "Codec.map"
+            [ roundTripTest
+                (Fuzz.intRange -10000 10000)
+                (Codec.map (\x -> x - 1) (\x -> x + 1) Codec.int)
+            ]
+        , Test.test "Codec.succeed"
+            (\_ ->
+                Expect.equal
+                    (Ok 632)
+                    (Codec.decodeString (Codec.succeed 632) "{}")
             )
-          <|
-            Codec.maybe Codec.int
         ]
 
-    {-
-       This is a known limitation: using null as Nothing and identity as Just means that nesting two maybes squashes Just Nothing with Nothing
-       , describe "double"
-          [ roundtrips
-              (Fuzz.oneOf
-                  [ Fuzz.constant Nothing
-                  , Fuzz.constant <| Just Nothing
-                  , Fuzz.map (Just << Just) Fuzz.int
-                  ]
-              )
-            <|
-              Codec.maybe <|
-                  Codec.maybe Codec.int
-          ]
-    -}
-    ]
 
 
-recursiveTests : List Test
-recursiveTests =
-    [ describe "list"
-        [ roundtrips (Fuzz.list Fuzz.int) <|
-            Codec.recursive
-                (\c ->
-                    Codec.custom
-                        (\fempty fcons value ->
-                            case value of
-                                [] ->
-                                    fempty
-
-                                x :: xs ->
-                                    fcons x xs
-                        )
-                        |> Codec.variant0 "[]" []
-                        |> Codec.variant2 "(::)" (::) Codec.int c
-                        |> Codec.buildCustom
-                )
-        ]
-    ]
+--
 
 
-mapAndThenTests : List Test
-mapAndThenTests =
-    [ describe "Codec.map"
-        [ roundtrips (Fuzz.intRange -10000 10000) <|
-            Codec.map (\x -> x - 1) (\x -> x + 1) Codec.int
-        ]
-    ]
+type SomeType a
+    = SomeType a
+
+
+
+--
+
+
+roundTripTest : Fuzz.Fuzzer a -> Codec.Codec a -> Test.Test
+roundTripTest fuzzer a =
+    Test.fuzz
+        fuzzer
+        "round trip"
+        (\x ->
+            Expect.equal (Ok x) (x |> Codec.encodeToValue a |> Codec.decodeValue a)
+        )
+
+
+
+--
+
+
+dictFuzzer : Fuzz.Fuzzer comparable -> Fuzz.Fuzzer v -> Fuzz.Fuzzer (Dict.Dict comparable v)
+dictFuzzer k v =
+    Fuzz.map2 Tuple.pair k v |> Fuzz.list |> Fuzz.map Dict.fromList
+
+
+setFuzzer : Fuzz.Fuzzer comparable -> Fuzz.Fuzzer (Set.Set comparable)
+setFuzzer a =
+    Fuzz.list a |> Fuzz.map Set.fromList
